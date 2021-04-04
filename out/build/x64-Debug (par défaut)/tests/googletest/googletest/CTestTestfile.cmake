2e40a9ecceacda7b5947f88b445ed7d4800520cb
Gryf <hajjarjamil4@gmail.com>
2021-04-04 22:19:58 +0200

    remove out folder

--- out/build/x64-Debug (par défaut)/tests/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Jamil/source/repos/Argentium/Argentium/tests/googletest/googletest
-# Build directory: C:/Users/Jamil/source/repos/Argentium/Argentium/out/build/x64-Debug (par défaut)/tests/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
