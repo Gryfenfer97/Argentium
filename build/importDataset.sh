50661880d064baec06995b78dc6ed24722d7ec0c
Gryf <hajjarjamil4@gmail.com>
2021-04-06 08:57:56 +0200

    edit immportDataset.sh

--- build/importDataset.sh
+++ build/importDataset.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
-
-# MNIST
-train_images_url="yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz"
-train_labels_url="yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz"
-test_images_url="yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz"
-test_labels_url="yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz"
-
-
-mkdir "./tests/dataset_tests/datasets/mnist/{train, test}"
-
-curl -o ./tests/dataset_tests/datasets/mnist/train/train-images-idx3-ubyte.gz $train_images_url
-gzip -d ./tests/dataset_tests/datasets/mnist/train/train-images-idx3-ubyte.gz
-curl -o ./tests/dataset_tests/datasets/mnist/train/train-labels-idx1-ubyte.gz $train_labels_url
-gzip -d ./tests/dataset_tests/datasets/mnist/train/train-labels-idx1-ubyte.gz
-
-curl -o ./tests/dataset_tests/datasets/mnist/test/t10k-images-idx3-ubyte.gz $test_images_url
-gzip -d ./tests/dataset_tests/datasets/mnist/test/t10k-images-idx3-ubyte.gz
-curl -o ./tests/dataset_tests/datasets/mnist/test/t10k-labels-idx1-ubyte.gz $test_labels_url
-gzip -d ./tests/dataset_tests/datasets/mnist/test/t10k-labels-idx1-ubyte.gz
+rm -rf "tests/dataset_tests"
+git clone --no-tags --depth 1 "https://github.com/MatthieuHernandez/Datasets-for-Machine-Learning.git" ./tests/dataset_tests
