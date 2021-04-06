14e45f85d86c668dabe41f0f564900d7eef1f8d3
Gryf <hajjarjamil4@gmail.com>
2021-04-06 09:48:51 +0200

    add ./ in filename in dataset test

--- build/importDataset.sh
+++ build/importDataset.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rm -rf "tests/dataset_tests/datasets"
+rm -r tests/dataset_tests/datasets/
 git clone --no-tags --depth 1 "https://github.com/MatthieuHernandez/Datasets-for-Machine-Learning.git" ./tests/dataset_tests/datasets
