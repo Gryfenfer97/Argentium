2e0532ba347e05c0e3de6ca6a7d63b286330f53e
Gryfenfer97 <hajjarjamil4@gmail.com>
2022-12-27 17:51:55 +0100

    update README

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # Argentium
 ![](https://github.com/Gryfenfer97/Argentium/workflows/dataset_test/badge.svg?barnch=master)
 
-A remake of Bromure, A deep learning laboratory
-
 Argentium is a deep learning library in c++17. The goal is to have a library powerful enough for a simple use like in a OCR projects, but also scalable to allow everyone to test a new feature in a neural network without recoding everything.
 
 ## How to use
@@ -16,25 +14,25 @@ Here is an example for MNIST dataset
 
 int main(){
   Ag::DataSetModel::MNIST trainData(
-    std::string("./datasets/MNIST/train-labels.idx1-ubyte"),
-    std::string("./datasets/MNIST/train-images.idx3-ubyte")
+    "./datasets/MNIST/train-labels.idx1-ubyte",
+    "./datasets/MNIST/train-images.idx3-ubyte"
   ); //load the training data
-  
-  std::vector<std::shared_ptr<Ag::LayerFactory>> topology = {
+
+  Ag::Topology topology = {
         Ag::InputLayer(784),
         Ag::FullyConnectedLayer(230,Ag::Activation::tanh),
         Ag::FullyConnectedLayer(10,Ag::Activation::sigmoid)
   }; // Create the topology for your network
-  
+
   Ag::Network net{ topology }; // Generate the network
   net.train(trainData, 1); //training with epoch = 1
 
   Ag::DataSetModel::MNIST testData(
-    std::string("./datasets/MNIST/t10k-labels.idx1-ubyte"),
-    std::string("./datasets/MNIST/t10k-images.idx3-ubyte")
+    "./datasets/MNIST/t10k-labels.idx1-ubyte",
+    "./datasets/MNIST/t10k-images.idx3-ubyte"
   ); // load the testing data
   const auto accuracy = net.test(testData); // test
-  
+
   std::cout << "Accuracy : " << accuracy << std::endl;
 }
 ```
--- tests/dataset_tests/main.cpp
+++ tests/dataset_tests/main.cpp
@@ -1,8 +1,8 @@
+#include <chrono>
 #include <gtest/gtest.h>
 #include <Argentium/Network/Network.hpp>
 #include <Argentium/Dataset.hpp>
 #include <Argentium/DatasetModel/MNIST.hpp>
-#include <chrono>
 #include <Argentium/Network/Input.hpp>
 #include <Argentium/Network/FullyConnected.hpp>
 #include <Argentium/Network/Activation.hpp>
@@ -10,79 +10,45 @@
 TEST(Dataset, xor)
 {
     Ag::DataSet data;
-    data.push({ 0., 0. }, { 0. });
-    data.push({ 0., 1. }, { 1. });
-    data.push({ 1., 0. }, { 1. });
-    data.push({ 1., 1. }, { 0. });
-    const std::vector<unsigned> topology = { 2,4,4,1 };
+    data.push({0., 0.}, {0.});
+    data.push({0., 1.}, {1.});
+    data.push({1., 0.}, {1.});
+    data.push({1., 1.}, {0.});
+    const std::vector<unsigned> topology = {2, 4, 4, 1};
 
-	
     std::vector<std::shared_ptr<Ag::LayerBuilder>> t = {
-    	Ag::FullyConnectedLayer(2,Ag::Activation::sigmoid),
-    	Ag::FullyConnectedLayer(4,Ag::Activation::sigmoid),
-        Ag::FullyConnectedLayer(4,Ag::Activation::sigmoid),
-        Ag::FullyConnectedLayer(1,Ag::Activation::sigmoid)
+        Ag::FullyConnectedLayer(2, Ag::Activation::sigmoid),
+        Ag::FullyConnectedLayer(4, Ag::Activation::sigmoid),
+        Ag::FullyConnectedLayer(4, Ag::Activation::sigmoid),
+        Ag::FullyConnectedLayer(1, Ag::Activation::sigmoid)
 
     };
 
     Ag::Network net(t);
 
-	
     net.train(data, 150000);
 
-	
     const auto successRate = net.test(data);
-    EXPECT_EQ(successRate, 1);
-    //EXPECT_EQ(1, 1);
+    EXPECT_EQ(successRate, 1.f);
 }
 
 TEST(Dataset, mnist)
 {
+    using namespace std::literals::string_literals;
+
     std::chrono::time_point<std::chrono::system_clock> start, end;
     start = std::chrono::system_clock::now();
-    Ag::DataSetModel::MNIST trainData(std::string("./datasets/MNIST/train-labels.idx1-ubyte"), std::string("./datasets/MNIST/train-images.idx3-ubyte"));
+    Ag::DataSetModel::MNIST trainData("./datasets/MNIST/train-labels.idx1-ubyte"s, "./datasets/MNIST/train-images.idx3-ubyte"s);
     std::vector<std::shared_ptr<Ag::LayerBuilder>> topology = {
         Ag::InputLayer(784),
-        Ag::FullyConnectedLayer(230,Ag::Activation::tanh),
-        Ag::FullyConnectedLayer(10,Ag::Activation::sigmoid)
-    };
+        Ag::FullyConnectedLayer(230, Ag::Activation::tanh),
+        Ag::FullyConnectedLayer(10, Ag::Activation::sigmoid)};
 
-    Ag::Network net{ topology };
-
-    net.train(trainData, 1);
-
-
-    Ag::DataSetModel::MNIST testData(std::string("./datasets/MNIST/t10k-labels.idx1-ubyte"), std::string("./datasets/MNIST/t10k-images.idx3-ubyte"));
-    
-    const auto successRate = net.test(testData);
-	end = std::chrono::system_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end - start;
-    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-
-    std::cout << "finished computation at " << std::ctime(&end_time)
-        << "elapsed time: " << elapsed_seconds.count() << "s\n";
-    std::cout << "successRate : " << successRate << std::endl;
-    EXPECT_GT(successRate, 0.90f);
-}
-
-
-TEST(Dataset, fashionMnist)
-{
-    std::chrono::time_point<std::chrono::system_clock> start, end;
-    start = std::chrono::system_clock::now();
-	Ag::DataSetModel::MNIST trainData(std::string("./datasets/Fashion-MNIST/train-labels-idx1-ubyte"), std::string("./datasets/Fashion-MNIST/train-images-idx3-ubyte"));
-    const std::vector<std::shared_ptr<Ag::LayerBuilder>> topology = {
-        Ag::InputLayer(784),
-        Ag::FullyConnectedLayer(230,Ag::Activation::sigmoid),
-        Ag::FullyConnectedLayer(10,Ag::Activation::softmax)
-    };
-
-    Ag::Network net{ topology };
+    Ag::Network net{topology};
 
     net.train(trainData, 1);
 
-
-    Ag::DataSetModel::MNIST testData(std::string("./datasets/Fashion-MNIST/t10k-labels-idx1-ubyte"), std::string("./datasets/Fashion-MNIST/t10k-images-idx3-ubyte"));
+    Ag::DataSetModel::MNIST testData("./datasets/MNIST/t10k-labels.idx1-ubyte"s, "./datasets/MNIST/t10k-images.idx3-ubyte"s);
 
     const auto successRate = net.test(testData);
     end = std::chrono::system_clock::now();
@@ -90,7 +56,36 @@ TEST(Dataset, fashionMnist)
     std::time_t end_time = std::chrono::system_clock::to_time_t(end);
 
     std::cout << "finished computation at " << std::ctime(&end_time)
-        << "elapsed time: " << elapsed_seconds.count() << "s\n";
+              << "elapsed time: " << elapsed_seconds.count() << "s\n";
+    std::cout << "successRate : " << successRate << std::endl;
+    EXPECT_GT(successRate, 0.90f);
+}
+
+TEST(Dataset, fashionMnist)
+{
+    using namespace std::literals::string_literals;
+
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    start = std::chrono::system_clock::now();
+    Ag::DataSetModel::MNIST trainData("./datasets/Fashion-MNIST/train-labels-idx1-ubyte"s, "./datasets/Fashion-MNIST/train-images-idx3-ubyte"s);
+    const std::vector<std::shared_ptr<Ag::LayerBuilder>> topology = {
+        Ag::InputLayer(784),
+        Ag::FullyConnectedLayer(230, Ag::Activation::sigmoid),
+        Ag::FullyConnectedLayer(10, Ag::Activation::softmax)};
+
+    Ag::Network net{topology};
+
+    net.train(trainData, 1);
+
+    Ag::DataSetModel::MNIST testData("./datasets/Fashion-MNIST/t10k-labels-idx1-ubyte"s, "./datasets/Fashion-MNIST/t10k-images-idx3-ubyte"s);
+
+    const auto successRate = net.test(testData);
+    end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+
+    std::cout << "finished computation at " << std::ctime(&end_time)
+              << "elapsed time: " << elapsed_seconds.count() << "s\n";
     std::cout << "successRate : " << successRate << std::endl;
     EXPECT_GT(successRate, 0.75f);
 }
@@ -125,9 +120,9 @@ TEST(Dataset, fashionMnist)
     EXPECT_GT(successRate, 0.70f);
 }*/
 
+int main(int argc, char *argv[])
+{
 
-int main(int argc, char* argv[]){
-
-	testing::InitGoogleTest(&argc, argv);
+    testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
